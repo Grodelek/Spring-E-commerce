a4dab141d149a5cc5d6fe15640f2a699a8d560ea
Grodelek <arczigrodel@wp.pl>
2024-11-02 14:42:53 +0100

    Custom Login form added

--- src/main/java/org/project/ecommerce/security/SecurityConfig.java
+++ src/main/java/org/project/ecommerce/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package org.project.ecommerce.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -24,5 +26,20 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(customer, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+        )
+                .formLogin(form ->
+                                form.loginPage("/login")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                        .defaultSuccessUrl("/", true)
+                                .permitAll()
+                );
+    return http.build();
     }
 }
