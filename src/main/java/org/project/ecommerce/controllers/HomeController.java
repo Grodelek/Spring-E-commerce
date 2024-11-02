8aa3575d09a22d213351f7bfe0aaea0492ed03ad
Grodelek <arczigrodel@wp.pl>
2024-11-02 22:58:02 +0100

    username preview on navbar

--- src/main/java/org/project/ecommerce/controllers/HomeController.java
+++ src/main/java/org/project/ecommerce/controllers/HomeController.java
@@ -1,16 +1,22 @@
 package org.project.ecommerce.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController{
 
-    @GetMapping("/")
-    public String home(){
+    @GetMapping
+    public String home(Model model, Authentication authentication) {
+        if (authentication != null && authentication.isAuthenticated()) {
+            model.addAttribute("username", authentication.getName());
+        } else {
+            model.addAttribute("username", "anonymous");
+        }
         return "homepage";
     }
 }
--- src/main/java/org/project/ecommerce/controllers/LoginController.java
+++ src/main/java/org/project/ecommerce/controllers/LoginController.java
@@ -1,5 +1,4 @@
 package org.project.ecommerce.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
